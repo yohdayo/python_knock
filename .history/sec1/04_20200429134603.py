f061a0a762eb8ec1f48b11c34b9bb01d0423be51
yohdayo <pb4l1rft@s.okayama-u.ac.jp>
2020-05-01 18:54:05 +0900

    pushed from VScode

---
+++ .history/sec1/04_20200429134603.py
@@ -0,0 +1,9 @@
+def sprit(string):
+    words = string.split(' ')
+
+def make_ngram(sequence, n):
+　   
+
+if __name__ == '__main__':
+    string = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+    sprit(string)
