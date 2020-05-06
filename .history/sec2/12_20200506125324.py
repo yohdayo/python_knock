be071383c07369c8481ba2c2df65a8769ca4e973
yohdayo <pb4l1rft@s.okayama-u.ac.jp>
2020-05-06 13:01:14 +0900

    コードクローンできてる

---
+++ .history/sec2/12_20200506125324.py
@@ -0,0 +1,28 @@
+def tab_to_space(file_name):
+    with open(file_name, 'r') as f:
+        result = []
+        for line in f:
+            ex =line.replace("\t", " ")
+            result.append(ex)
+
+    return result
+
+def save_col1(lines, outfile):
+    with open(outfile, 'w') as f:
+        for line in lines:
+            sp_line = line.split(" ")
+            f.write(sp_line[0]+"\n")
+
+def save_col2(lines, outfile):
+    with open(outfile, 'w') as f:
+        for line in lines:
+            sp_line = line.split(" ")
+            f.write(sp_line[1]+"\n")
+    
+if __name__ == '__main__':
+    file_name = '../datas/hightemp.txt'
+    exchanged = tab_to_space(file_name)
+    out_file1 = '.col1.txt'
+    out_file2 = '.col2.txt'
+    save_col1(exchanged,out_file1)
+    save_col2(exchanged,out_file2)
---
+++ .history/sec2/13_20200506114559.py
@@ -0,0 +1,8 @@
+def merge_files(file1, file2):
+
+
+if __name__ == '__main__':
+    file1 = 'col1.txt'
+    file2 = 'col2.txt'
+    out_file = 'merged.txt'
+    merge_files(file1,file2)
