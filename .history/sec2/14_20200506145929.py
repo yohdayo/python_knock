11a0592e521ee280414002af6ff1ba7541057c49
yohdayo <pb4l1rft@s.okayama-u.ac.jp>
2020-05-06 14:59:42 +0900

    a

---
+++ .history/sec2/14_20200506145929.py
@@ -0,0 +1,12 @@
+import sys
+
+def print_lines(filename, n):
+    
+    
+
+if __name__ == '__main__':
+    file_name = '../datas/hightemp.txt'
+    args = sys.argv
+    n = args[1]
+
+    print_lines(filename, n)
